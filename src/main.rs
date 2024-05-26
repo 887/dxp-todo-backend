6b231518a8f9f0559f7ba0d90d9fb279f5dcc3d8
887 <2300887@gmail.com>
2024-05-26 11:43:09 +0200

    no except

--- src/main.rs
+++ src/main.rs
@@ -31,10 +31,10 @@ async fn main() -> std::io::Result<()> {
     #[cfg(all(feature = "path-info"))]
     println!("lib path {}", get_lib_path());
 
-    //this channel is for lib reloads
+    //this channel is for lib reloads. it tells the main runtime when to do a reload
     let (tx_lib_reloaded, mut rx_lib_reloaded) = mpsc::channel(1);
 
-    //this ensures this will only be dropped when the main runtime is shut down
+    //this task observes the lib reloads
     tokio::task::spawn(async move {
         loop {
             wait_for_reload(tx_lib_reloaded.clone()).await;
@@ -56,6 +56,8 @@ async fn main() -> std::io::Result<()> {
         //everything that can fail needs to be in this task
         //once this task finishes the hot-reload-lib checks if there is a new library to reload
         let main_loop = tokio::task::spawn(async move {
+            println!("------------main Task------------");
+
             let wait = async move {
                 println!("trying again in 3s");
                 tokio::time::sleep(std::time::Duration::from_secs(3)).await;
@@ -66,8 +68,6 @@ async fn main() -> std::io::Result<()> {
                 wait.await;
                 return
             }
-
-            println!("-----------------------------------");
 
             //using threads here causes panics
             //https://stackoverflow.com/questions/62536566/how-can-i-create-a-tokio-runtime-inside-another-tokio-runtime-without-getting-th
@@ -119,7 +119,7 @@ async fn main() -> std::io::Result<()> {
         });
 
         tokio::select! {
-            // This simulates the normal main loop behavior...
+            // This simulates the normal main loop behavior
             _ = main_loop => {
             }
 
@@ -127,6 +127,7 @@ async fn main() -> std::io::Result<()> {
             // blocked while the token is still in scope. This gives us the
             // control over how long the reload should wait.
             Some(block_reload_token) = rx_lib_reloaded.recv() => {
+                println!("------------lib reload------------");
 
                 //signal sever to shutdown
                 if *server_running_check.read().await {
@@ -135,18 +136,12 @@ async fn main() -> std::io::Result<()> {
                         Ok(_) => {
                             if *server_running_check.read().await {
                                 match rx_server_was_shutdown.recv().await {
-                                    Some(_) => {
-                                        println!("received server_was_shutdown signal");
-                                    }
-                                    None => {
-                                        println!("server_was_shutdown listening channel closed");
-                                    }
+                                    Some(_) => { println!("received server_was_shutdown signal"); }
+                                    None => { println!("server_was_shutdown listening channel closed"); }
                                 }
                             }
                         }
-                        Err(e) => {
-                            println!("error sending shutdown signal: {}", e);
-                        }
+                        Err(e) => { println!("error sending shutdown signal: {}", e); }
                     }
                 }
 
@@ -157,10 +152,20 @@ async fn main() -> std::io::Result<()> {
 }
 
 async fn wait_for_reload(tx_lib_reloaded: Sender<BlockReload>) -> () {
-    let block_reload = spawn_blocking(|| hot_lib::subscribe().wait_for_about_to_reload())
-        .await
-        .expect("get token");
-    tx_lib_reloaded.send(block_reload).await.expect("send token");
+    let block_reload_result = 
+        spawn_blocking(|| hot_lib::subscribe().wait_for_about_to_reload())
+        .await;
+    let block_reload = match block_reload_result {
+        Ok(br) => br,
+        Err(err) => { 
+            println!("wait_for_about_to_reload error: {:?}", err);
+            return;
+        }
+    };
+    match tx_lib_reloaded.send(block_reload).await {
+        Ok(_) => { }
+        Err(e) => { println!("error sending server_was_shutdown signal: {:?}", e);}
+    }
 }
 
 fn run_migration() -> Result<(), anyhow::Error> {
@@ -173,16 +178,20 @@ fn run_server(rx_shutdown_server: Receiver<()>) -> Result<(), anyhow::Error> {
 
 async fn do_reload(block_reload_token: BlockReload) {
 
-    println!("...now we are ready for reloading...");
     // Now drop the token, allow the reload
     drop(block_reload_token); // token drop causes reload to continue
 
     println!("trying reload");
     // Now we wait for the lib to be reloaded...
-    spawn_blocking(|| hot_lib::subscribe().wait_for_reload())
-        .await
-        .expect("wait for reload");
-    println!("...now we have the new library version loaded");
+    let reload_result =
+        spawn_blocking(|| hot_lib::subscribe().wait_for_reload())
+        .await;
+    match reload_result {
+        Ok(_) => { println!("reload successful") }
+        Err(err) => { println!("reload error: {:?}", err) }
+    }
+
+    //println!("...now we have the new library version loaded");
 
     // now main loop with tokio::select! continues and restarts all the primary futures
 }
