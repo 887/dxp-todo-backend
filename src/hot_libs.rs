9f521862f7ec4883aca6947c5cb058a3484f1f01
887 <2300887@gmail.com>
2024-05-29 00:23:14 +0200

    hot_libs: info about reload

--- src/hot_libs.rs
+++ src/hot_libs.rs
@@ -1,5 +1,8 @@
+//info: in order to cause a reload you nee to actually change a function signature/make the compiler do work
+//if the file is identical to the compiler, hot-reload will not try to do a reload
+
 #[cfg(feature = "hot-reload")]
-#[hot_lib_reloader::hot_module(dylib = "lib")]
+#[hot_lib_reloader::hot_module(dylib = "lib", file_watch_debounce = 10)]
 pub(crate) mod hot_lib {
     // pub use lib::*;
 
@@ -11,7 +14,7 @@ pub(crate) mod hot_lib {
 }
 
 #[cfg(feature = "hot-reload")]
-#[hot_lib_reloader::hot_module(dylib = "migration_runner")]
+#[hot_lib_reloader::hot_module(dylib = "migration_runner", file_watch_debounce = 10)]
 pub(crate) mod hot_migration_runner {
     // pub use migration_runner::*;
 
