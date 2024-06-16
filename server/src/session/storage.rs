fe0e45076b1a4a9db9499e62b53642783252dcc3
887 <2300887@gmail.com>
2024-06-16 18:30:44 +0200

    storage methods similar

--- server/src/session/storage.rs
+++ server/src/session/storage.rs
@@ -7,12 +7,12 @@
     feature = "postgres-native-tls",
     feature = "redis"
 )))]
-fn get_storage() -> anyhow::Result<poem::session::MemoryStorage> {
+pub async fn get_storage() -> anyhow::Result<poem::session::MemoryStorage> {
     Ok(poem::session::MemoryStorage::new())
 }
 
 #[cfg(feature = "redis")]
-pub async fn get_redis_storage() -> anyhow::Result<poem::session::RedisStorage> {
+pub async fn get_storage() -> anyhow::Result<poem::session::RedisStorage> {
     let redis_url = env::var("REDIS_URL").context("REDIS_URL is not set")?;
     let client = redis::Client::open(redis_url)?;
     let con_manager = redis::aio::ConnectionManager::new(client).await?;
