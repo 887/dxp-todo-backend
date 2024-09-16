7990bef047ebf0f51ef77f88df5c4ca3f6f3aab0
887 <2300887@gmail.com>
2024-09-17 00:48:59 +0200

    axum session

--- migration/src/lib.rs
+++ migration/src/lib.rs
@@ -8,7 +8,7 @@ pub struct Migrator;
 impl MigratorTrait for Migrator {
     fn migrations() -> Vec<Box<dyn MigrationTrait>> {
         vec![
-            Box::new(dxp_db_session::migration::Migration),
+            Box::new(dxp_axum_session::migration::Migration),
             Box::new(m20220101_000001_create_table::Migration),
         ]
     }
