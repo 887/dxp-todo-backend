162f769390689310e0fd718b46d5b8f40724a64b
887 <2300887@gmail.com>
2024-10-19 23:14:21 +0200

    utoipa old for backend

--- heart/src/routes/index/mod.rs
+++ heart/src/routes/index/mod.rs
@@ -4,24 +4,31 @@ use crate::session::SessionType;
 
 pub static SESSION_INDEX_COUNTER: &str = "index_counter";
 
-pub async fn index(session: SessionType) -> String {
-    let mut counter = session.get::<usize>(SESSION_INDEX_COUNTER).unwrap_or(0);
-    counter += 1;
+// pub async fn index(session: SessionType) -> String {
+//     let mut counter = session.get::<usize>(SESSION_INDEX_COUNTER).unwrap_or(0);
+//     counter += 1;
 
-    session.set(SESSION_INDEX_COUNTER, counter);
+//     session.set(SESSION_INDEX_COUNTER, counter);
 
-    let hello = format!(
-        "hello world! {}\r\nSession ID: {}",
-        counter,
-        session.get_session_id().inner()
-    );
+//     let hello = format!(
+//         "hello world! {}\r\nSession ID: {}",
+//         counter,
+//         session.get_session_id().inner()
+//     );
+//     trace!("{}", &hello);
+
+//     hello
+// }
+
+pub async fn index() -> String {
+    let hello = "hello world 1!".to_string();
     trace!("{}", &hello);
 
     hello
 }
 
 pub async fn index2() -> String {
-    let hello = "hello world!".to_string();
+    let hello = "hello world 2!".to_string();
     trace!("{}", &hello);
 
     hello
